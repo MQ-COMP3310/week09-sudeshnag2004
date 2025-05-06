f2b2a86f78dc59b21a45100b21b98d379e8aea96
Sudeshna Gareshe <sudeshna.gareshe@students.mq.edu.au>
2025-05-06 18:45:26 +1000

    fixed vulnarabilities

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from project import create_app
+import os 
 
 if __name__ == '__main__':
   app = create_app()
-  app.run(host = '0.0.0.0', port = 8001, debug=True) 
-
+  debug_mode = os.getenv('FLASK_ENV', 'production') == 'development'
+  app.run(host='127.0.0.1', port=8001, debug=debug_mode)
