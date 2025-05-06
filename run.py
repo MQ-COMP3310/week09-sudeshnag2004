5a6de39b34c6e9cb9f0db942499ac7c08e394128
Sudeshna Gareshe <sudeshna.gareshe@students.mq.edu.au>
2025-05-06 18:49:26 +1000

    initial commit

--- run.py
+++ run.py
@@ -3,5 +3,4 @@ import os
 
 if __name__ == '__main__':
   app = create_app()
-  debug_mode = os.getenv('FLASK_ENV', 'production') == 'development'
-  app.run(host='127.0.0.1', port=8001, debug=debug_mode)
+  app.run(host='127.0.0.1', port=8001, debug=False)
